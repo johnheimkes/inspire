8af660ad059bfe990d4dade1582f10cf210ec21d
Jake Dahn <admin@jakedahn.com>
2010-09-19 03:45:52 -0500

    vanilla rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inspirempls::Application.config.secret_token = '0b940e4e69b9b3e41ceac1a5d96d9568147a6ff03a781eac303a6bb3325cf37434d770e9805a2120de97668110c51f20d94f5b0d4a389d81a9e8fdc4586b7653'
