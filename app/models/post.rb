7910ac49688e99e5be426f79aa1d5f1035a9cda7
Jake Dahn <admin@jakedahn.com>
2010-09-24 03:15:43 -0500

    paperclip is playing mostly nicely with s3

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,13 @@ class Post < ActiveRecord::Base
   cattr_reader :per_page
   @@per_page = 10
   
-  has_attached_file :image, :styles => { :medium => "800x600>", :thumb => "50x50!" }
+  has_attached_file :image, 
+                    :styles => { :medium => "800x600>", :thumb => "50x50" },
+                    :storage => :s3, 
+                    :s3_credentials => { :access_key_id => ENV['S3_KEY'],
+                                         :secret_access_key => ENV['S3_SECRET'],
+                                         :bucket => "images.inspirempls.com" },
+                    :path => "/:style/:filename"
+                    
   
 end
