d20230925a8f87cc51f8ae642b18bf3794e9736a
Jake Dahn <admin@jakedahn.com>
2010-09-24 18:14:03 -0500

    refactoring css for pagination

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   # attr_accessible :image_url, :title, :body, :user_id
   
   cattr_reader :per_page
-    @@per_page = 2
+    @@per_page = 10
   
   has_attached_file :image, 
                     :styles => { :medium => "800x600>", :thumb => "50x50" },
