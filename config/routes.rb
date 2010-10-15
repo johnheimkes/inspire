6c0045c99f108d5b64d184a782f45df5fdf6d027
Jake Dahn <admin@jakedahn.com>
2010-10-14 23:35:48 -0500

    adding splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Inspirempls::Application.routes.draw do
+  get "splash/index"
+
   get "user/index"
 
   devise_for :user do
@@ -64,7 +66,8 @@ Inspirempls::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "posts#index"
+  # root :to => "posts#index"
+  root :to => "splash#index"
 
   # See how all your routes lay out with "rake routes"
 
