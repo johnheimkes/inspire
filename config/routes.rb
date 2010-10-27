347bf35d9fac554ee47fc93699fd88896ae5719c
Jake Dahn <admin@jakedahn.com>
2010-10-27 18:35:21 -0500

    finished omniauth i think?

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Inspirempls::Application.routes.draw do
 
   match '/auth/:provider/callback' => 'authentications#create'  
-  devise_for :users  
+  devise_for :users, :controllers => { :registrations => 'registrations' }  
   resources :posts
   resources :authentications
   
@@ -17,5 +17,6 @@ Inspirempls::Application.routes.draw do
 
   root :to => "posts#index"
   # root :to => "splash#index"
+
 
 end
