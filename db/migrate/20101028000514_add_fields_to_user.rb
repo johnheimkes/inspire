6b77deb84ecfc4a1c2755c329db555c8c58089bd
Jake Dahn <admin@jakedahn.com>
2010-10-30 02:10:16 -0500

    tweaking migration

--- db/migrate/20101028000514_add_fields_to_user.rb
+++ db/migrate/20101028000514_add_fields_to_user.rb
@@ -3,11 +3,13 @@ class AddFieldsToUser < ActiveRecord::Migration
     add_column :users, :name, :string
     add_column :users, :username, :string
     add_column :users, :bio, :text
+    add_column :users, :avatar, :string
   end
 
   def self.down
     remove_column :users, :bio
     remove_column :users, :username
     remove_column :users, :name
+    remove_column :users, :avatar
   end
 end
