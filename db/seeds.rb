cf29e6f0f806e6d47bbbe7038a965b022f0f7fa4
Jake Dahn <admin@jakedahn.com>
2010-10-30 04:28:56 -0500

    adjusting the signout text style, setting up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,29 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-# User.create(:name => nil, :encrypted_password => "", :created_at => "Fri Sep 24 11:39:04 UTC 2010", :updated_at => "Fri Sep 24 12:01:33 UTC 2010", :last_sign_in_ip => "127.0.0.1", :twitter_oauth_secret => "8vweA95Xey88NOjMIVjyel6LhX6WyOUpRlEInbokqg", :last_sign_in_at => "Fri Sep 24 11:56:32 UTC 2010", :sign_in_count => 6, :id => 8, :gender => nil, :password_salt => "", :twitter_handle => "jakedahn", :reset_password_token => nil, :remember_token => nil, :current_sign_in_ip => "127.0.0.1", :remember_created_at => nil, :current_sign_in_at => "Fri Sep 24 12:01:33 UTC 2010", :bio => nil, :twitter_oauth_token => "12653-Z9Nm4X3SHAqp66SGZto6D9LU5oRXqBmCvy8t7qIE", :email => "")
-# 
-# (1..30).each do |x|
-#   Post.create(
-#     :image_url => nil,
-#     :title => "asffada",
-#     :body => "foo",
-#     :user_id => 1,
-#     :created_at => "2010-09-24 22:33:02",
-#     :updated_at => "2010-09-24 22:33:02",
-#     :image_file_name => "Screen shot 2010-09-24 at 5.42.16 PM.png",
-#     :image_content_type => "image/png",
-#     :image_file_size => 382461,
-#     :image_updated_at => "2010-09-24 22:33:01"
-#   )
-# end
+(1..30).each do |x|
+  Post.create(
+    :image_url => nil,
+    :title => "asffada",
+    :body => "foo",
+    :user_id => 1,
+    :created_at => "2010-09-24 22:33:02",
+    :updated_at => "2010-09-24 22:33:02",
+    :image_file_name => "Screen shot 2010-09-24 at 5.42.16 PM.png",
+    :image_content_type => "image/png",
+    :image_file_size => 382461,
+    :image_updated_at => "2010-09-24 22:33:01"
+  )
+end
 
+a = Post.find(1)
+a.tag_list = "foo, bar, baz"
+a.save
+
+b = Post.find(2)
+b.tag_list = "foo, bar"
+b.save
+
+c = Post.find(3)
+c.tag_list = "foo"
+c.save
